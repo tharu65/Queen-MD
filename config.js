1428f1ba213d78182a7f5839d9bb9f83cf10f6e4
tharu65 <122668204+tharu65@users.noreply.github.com>
2023-01-31 23:36:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'c2dKdDNGOEo=',
+global.ownername = process.env.OWNER_NAME === undefined ? "BOT SHAGI" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94729595276' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
